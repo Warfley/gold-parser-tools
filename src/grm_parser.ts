f092e35e340912b138a8a9b07a6f9655a4cca0f1
Frederic Kehrein <frederic@kehrein.org>
2023-01-20 21:53:05 +0100

    Default set completion and Range Set support

--- src/grm_parser.ts
+++ src/grm_parser.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import internal = require("stream");
 import { Diagnostic, DiagnosticSeverity, Position, Range, TextDocument } from "vscode";
 
 export enum TokenType {UNKNOWN=0, TERMINAL, NON_TERMINAL, SET, PARAMETER, CONST_TERMINAL, OPERATOR, CONST_SET};
@@ -9,6 +8,7 @@ export interface GRMToken {
   value: string;
   type: TokenType;
   location: Position;
+  hint?: string;
 }
 
 interface GRMGrammarSymbols {
@@ -30,25 +30,202 @@ export interface GRMDefinition {
   type: DefinitionType;
 }
 
-function default_set(name: string): GRMToken {
+function default_set(name: string, hint?: string): GRMToken {
   return {
     value: name,
     type: TokenType.SET,
-    location: new Position(0, 0)
+    location: new Position(0, 0),
+    hint: hint
   };
 }
 
+/* Exctracted from http://goldparser.org/doc/grammars/index.htm with
+let s=""
+for (let tr of trs) {
+  let set = tr.children[0].innerText;
+  if (!set.startsWith("{")) continue;
+  let call = 'default_set("' + set + '"';
+  if (tr.children.length === 3) call += ', "' + tr.children[2].innerText + '"';
+  s += call + "),\n"
+}
+*/
+
 const DEFAULT_SETS: Array<GRMToken> = [
-  default_set("{Space}"),
-  default_set("{Digit}"),
-  default_set("{Letter}"),
-  default_set("{Printable}"),
-  default_set("{CR}"),
-  default_set("{LF}"),
-  default_set("{Alphanumeric}"),
-  default_set("{Whitespace}"),
-  default_set("{VT}"),
-  default_set("{HT}")
+default_set("{HT}", "Horizontal Tab character."),
+default_set("{LF}", "Line Feed character."),
+default_set("{VT}", "Vertical Tab character. This character is rarely used."),
+default_set("{FF}", "Form Feed character. This character is also known as 'New Page'."),
+default_set("{CR}", "Carriage Return character {#13}."),
+default_set("{Space}", "Space character. Technically, this set is not needed since a 'space' can be expressed by using single quotes: ' '. The set was added to allow the developer to more explicitly indicate the character and add readability."),
+default_set("{NBSP}", "No-Break Space character. The No-Break Space character is used to represent a space where a line break is not allowed. It is often used in source code for indentation."),
+default_set("{LS}", "Unicode Line Separator"),
+default_set("{PS}", "Unicode Paragraph Separator"),
+default_set("{Number}", "0123456789"),
+default_set("{Digit}", "0123456789\nThis set is maintained to support older grammars. The term 'digit' is technically inaccurate and this set will eventually be removed, but not for a long time. Please use the {Number} set."),
+default_set("{Letter}", "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+default_set("{AlphaNumeric}", "This set includes all the characters in {Letter} and {Number}"),
+default_set("{Printable}", "This set includes all standard characters that can be printed onscreen. This includes the characters from  #32 to #127 and  #160 (No-Break Space). The No-Break Space character was included since it is often used in source code."),
+default_set("{Letter Extended}", "This set includes all the letters which are part of the extended characters in the first 256 characters (ANSI)."),
+default_set("{Printable Extended}", "This set includes all the printable characters above #127. Although rarely used in programming languages, they could be used, for instance, as valid characters in a string literal."),
+default_set("{Whitespace}", "This set includes all characters that are normally considered whitespace and ignored by the parser. The set consists of the Space, Horizontal  Tab, Line Feed, Vertical Tab, Form Feed, Carriage Return and No-Break Space."),
+default_set("{All Valid}", "The {All Valid} character set contains every valid character in the Basic Multilingual Plane of the Unicode Character Set. This includes the characters from &1 to &D7FF and  &DC00 to &FFEF.\n Please note that this set also includes whitespace and control characters that are rarely used in programming languages.  It is only available in versions 2.6 and later of the Builder."),
+default_set("{ANSI Mapped}", "This set contains the characters between 128 and 159 that have different values in Unicode. The set is only available in versions 2.0.6 and later of the Builder."),
+default_set("{ANSI Printable}", "This set contains all printable characters available in ANSI. Essentially, this is a union of {Printable}, {Printable Extended} and {ANSI Mapped}.  The set is only available in versions 2.0.6 and later of the Builder."),
+default_set("{Control Codes}", "This set includes the characters from 1 to 31 and from 127 to 159.  It is only available in versions 2.6 and later of the Builder."),
+default_set("{Euro Sign}", "The Euro Currency Sign."),
+default_set("{Formatting}", "Unicode formatting characters."),
+default_set("{All Latin}"),
+default_set("{All Letters}"),
+default_set("{All Printable}"),
+default_set("{All Space}"),
+default_set("{All Newline}"),
+default_set("{All Whitespace}"),
+default_set("{Basic Latin}"),
+default_set("{Latin-1 Supplement}"),
+default_set("{Latin Extended-A}"),
+default_set("{Latin Extended-B}"),
+default_set("{IPA Extensions}"),
+default_set("{Spacing Modifier Letters}"),
+default_set("{Combining Diacritical Marks}"),
+default_set("{Greek and Coptic}"),
+default_set("{Cyrillic}"),
+default_set("{Cyrillic Supplement}"),
+default_set("{Armenian}"),
+default_set("{Hebrew}"),
+default_set("{Arabic}"),
+default_set("{Syriac}"),
+default_set("{Arabic Supplement}"),
+default_set("{Thaana}"),
+default_set("{NKo}"),
+default_set("{Samaritan}"),
+default_set("{Devanagari}"),
+default_set("{Bengali}"),
+default_set("{Gurmukhi}"),
+default_set("{Gujarati}"),
+default_set("{Oriya}"),
+default_set("{Tamil}"),
+default_set("{Telugu}"),
+default_set("{Kannada}"),
+default_set("{Malayalam}"),
+default_set("{Sinhala}"),
+default_set("{Thai}"),
+default_set("{Lao}"),
+default_set("{Tibetan}"),
+default_set("{Myanmar}"),
+default_set("{Georgian}"),
+default_set("{Hangul Jamo}"),
+default_set("{Ethiopic}"),
+default_set("{Ethiopic Supplement}"),
+default_set("{Cherokee}"),
+default_set("{Unified Canadian Aboriginal Syllabics}"),
+default_set("{Ogham}"),
+default_set("{Runic}"),
+default_set("{Tagalog}"),
+default_set("{Hanunoo}"),
+default_set("{Buhid}"),
+default_set("{Tagbanwa}"),
+default_set("{Khmer}"),
+default_set("{Mongolian}"),
+default_set("{Unified Canadian Aboriginal Syllabics Extended}"),
+default_set("{Limbu}"),
+default_set("{Tai Le}"),
+default_set("{New Tai Lue}"),
+default_set("{Khmer Symbols}"),
+default_set("{Buginese}"),
+default_set("{Tai Tham}"),
+default_set("{Balinese}"),
+default_set("{Sundanese}"),
+default_set("{Lepcha}"),
+default_set("{Ol Chiki}"),
+default_set("{Vedic Extensions}"),
+default_set("{Phonetic Extensions}"),
+default_set("{Phonetic Extensions Supplement}"),
+default_set("{Combining Diacritical Marks Supplement}"),
+default_set("{Latin Extended Additional}"),
+default_set("{Greek Extended}"),
+default_set("{General Punctuation}"),
+default_set("{Superscripts and Subscripts}"),
+default_set("{Currency Symbols}"),
+default_set("{Combining Diacritical Marks for Symbols}"),
+default_set("{Letterlike Symbols}"),
+default_set("{Number Forms}"),
+default_set("{Arrows}"),
+default_set("{Mathematical Operators}"),
+default_set("{Miscellaneous Technical}"),
+default_set("{Control Pictures}"),
+default_set("{Optical Character Recognition}"),
+default_set("{Enclosed Alphanumerics}"),
+default_set("{Box Drawing}"),
+default_set("{Block Elements}"),
+default_set("{Geometric Shapes}"),
+default_set("{Miscellaneous Symbols}"),
+default_set("{Dingbats}"),
+default_set("{Miscellaneous Mathematical Symbols-A}"),
+default_set("{Supplemental Arrows-A}"),
+default_set("{Braille Patterns}"),
+default_set("{Supplemental Arrows-B}"),
+default_set("{Miscellaneous Mathematical Symbols-B}"),
+default_set("{Supplemental Mathematical Operators}"),
+default_set("{Miscellaneous Symbols and Arrows}"),
+default_set("{Glagolitic}"),
+default_set("{Latin Extended-C}"),
+default_set("{Coptic}"),
+default_set("{Georgian Supplement}"),
+default_set("{Tifinagh}"),
+default_set("{Ethiopic Extended}"),
+default_set("{Cyrillic Extended-A}"),
+default_set("{Supplemental Punctuation}"),
+default_set("{CJK Radicals Supplement}"),
+default_set("{Kangxi Radicals}"),
+default_set("{Ideographic Description Characters}"),
+default_set("{CJK Symbols and Punctuation}"),
+default_set("{Hiragana}"),
+default_set("{Katakana}"),
+default_set("{Bopomofo}"),
+default_set("{Hangul Compatibility Jamo}"),
+default_set("{Kanbun}"),
+default_set("{Bopomofo Extended}"),
+default_set("{CJK Strokes}"),
+default_set("{Katakana Phonetic Extensions}"),
+default_set("{Enclosed CJK Letters and Months}"),
+default_set("{CJK Compatibility}"),
+default_set("{CJK Unified Ideographs Extension A}"),
+default_set("{Yijing Hexagram Symbols}"),
+default_set("{CJK Unified Ideographs}"),
+default_set("{Yi Syllables}"),
+default_set("{Yi Radicals}"),
+default_set("{Lisu}"),
+default_set("{Vai}"),
+default_set("{Cyrillic Extended-B}"),
+default_set("{Bamum}"),
+default_set("{Modifier Tone Letters}"),
+default_set("{Latin Extended-D}"),
+default_set("{Syloti Nagri}"),
+default_set("{Common Indic Number Forms}"),
+default_set("{Phags-pa}"),
+default_set("{Saurashtra}"),
+default_set("{Devanagari Extended}"),
+default_set("{Kayah Li}"),
+default_set("{Rejang}"),
+default_set("{Hangul Jamo Extended-A}"),
+default_set("{Javanese}"),
+default_set("{Cham}"),
+default_set("{Myanmar Extended-A}"),
+default_set("{Tai Viet}"),
+default_set("{Meetei Mayek}"),
+default_set("{Hangul Syllables}"),
+default_set("{Hangul Jamo Extended-B}"),
+default_set("{Private Use Area}"),
+default_set("{CJK Compatibility Ideographs}"),
+default_set("{Alphabetic Presentation Forms}"),
+default_set("{Arabic Presentation Forms-A}"),
+default_set("{Variation Selectors}"),
+default_set("{Vertical Forms}"),
+default_set("{Combining Half Marks}"),
+default_set("{CJK Compatibility Forms}"),
+default_set("{Small Form Variants}"),
+default_set("{Arabic Presentation Forms-B}"),
+default_set("{Halfwidth and Fullwidth Forms}"),
 ];
 
 export function token_name(token: GRMToken): string {
@@ -64,6 +241,7 @@ export function token_name(token: GRMToken): string {
 
 const advance_expr = new RegExp("!\\*|[^\\s]");
 const end_of_comment_expr = new RegExp("\\*!");
+const set_range_expr = new RegExp("\\s*(#\\d+|&[0-9a-fA-F]{1,4})(\\s*\\.\\.\\s*(#\\d+|&[0-9a-fA-F]{1,4}))?\\s*");
 // See language definition
 // Terminal Token: [A-Za-z0-9_.][A-Za-z0-9_.-]* // to not match (-) (not quite correct)
 // Non Terminal Token: <[A-Za-z0-9\s_.-]+>
@@ -446,6 +624,35 @@ export class DocumentParser {
     }
   }
 
+  private check_range_set(token: GRMToken): boolean {
+    if (token.type !== TokenType.SET) {
+      return false;
+    }
+    let name = token.value.substring(1, token.value.length-1);
+    let match = name.match(set_range_expr);
+    if (match === null || match[0].length !== name.length) {
+      return false;
+    }
+    let start_str = match[1];
+    let start = start_str.startsWith("#")
+              ? +start_str.substring(1)
+              : Number("0x" + start_str.substring(1));
+    let stop = start;
+    if (match[2] !== undefined) {
+      let stop_str = match[3];
+      stop = stop_str.startsWith("#")
+                ? +stop_str.substring(1)
+                : Number("0x" + stop_str.substring(1));
+      if (start > stop) {
+        this.token_error(token, "The start of a range must be smaller than then end");
+      }
+    }
+    if (start > 65519 || stop > 65519) {
+      this.token_error(token, "Range values out of the allowed character range for UTF-16");
+    }
+    return true;
+  }
+
   private check_errors() {
     // first collect all symbols
     for (let definition of this.definitions) {
@@ -496,6 +703,10 @@ export class DocumentParser {
       // parse other symbols:
       for (let i=start_index; i<definition.symbols.length; ++i) {
         let symbol = definition.symbols[i];
+        if (this.check_range_set(symbol)) {
+          // Treat range sets as constants and ignore them here
+          continue;
+        }
         // undefiled chain through ? skips if not found
         this.symbols.used_symbols.get(symbol.type)?.push(symbol);
       }
